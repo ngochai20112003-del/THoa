f065058b8eedf707cb7cb0825f708815c4fe729b
ngochai20112003-del <ngochai20112003@gmail.com>
2026-03-08 11:46:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Em nhé';
+const contentCard = 'Chúc cô gái đáng yêu này một ngày 8/3 thật nhiều niềm vui, luôn cười thật tươi vì nụ cười của em rất đẹp.';
+
+const giftImage = 'hoa3.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
